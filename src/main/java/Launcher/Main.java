d85a93b3785dd458b0559bddd47e52d6bf7f6258
Eduardo <ewaldo.jefe@gmail.com>
2025-06-20 19:58:19 -0400

    ConsolaColeccion v1

--- src/main/java/Launcher/Main.java
+++ src/main/java/Launcher/Main.java
@@ -1,4 +1,10 @@
 package Launcher;
 
+import Vista.ConsolaColeccion;
+
 public class Main {
+    public static void main(String[] args) {
+        ConsolaColeccion app = new ConsolaColeccion();
+        app.iniciar();
+    }
 }
